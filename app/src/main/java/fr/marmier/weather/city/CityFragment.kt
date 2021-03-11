bc8675a7ac3477042508861ab98ac77724bf5ca2
RexT2507 <baptiste.marmier@ynov.com>
2021-03-11 11:43:49 +0100

    [DEV] Création d'une ville avec Fragment

---
+++ app/src/main/java/fr/marmier/weather/city/CityFragment.kt
@@ -0,0 +1,20 @@
+package fr.marmier.weather.city
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import fr.marmier.weather.R
+
+class CityFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_city, container, false)
+        return view
+    }
+}
