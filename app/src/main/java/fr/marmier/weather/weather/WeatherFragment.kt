fb82f570d69b94b20beced9752d46a3f71d76033
Pierre ROMAN <flaeur@gmail.com>
2021-04-01 12:10:07 +0200

    [DEV] Création du weatherFragment

---
+++ app/src/main/java/fr/marmier/weather/weather/WeatherFragment.kt
@@ -0,0 +1,26 @@
+package fr.marmier.weather.weather
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import fr.marmier.weather.R
+
+class WeatherFragment : Fragment() {
+    companion object {
+        val EXTRA_CITY_NAME = "project.kotlin.weather.extras.EXTRA_CITY_NAME"
+
+        fun newInstance() = WeatherFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_weather, container, false)
+        return view
+    }
+
+}
