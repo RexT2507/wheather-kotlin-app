3ea90b372fbed04c9a21568e7cd905de4428906f
Pierre ROMAN <flaeur@gmail.com>
2021-04-08 10:53:27 +0200

    [FIX] Correction onCreate

--- app/src/main/java/fr/marmier/weather/weather/WeatherActivity.kt
+++ app/src/main/java/fr/marmier/weather/weather/WeatherActivity.kt
@@ -2,11 +2,17 @@ package fr.marmier.weather.weather
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class WeatherActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+//    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+//        super.onCreate(savedInstanceState, persistentState)
+//        supportFragmentManager.beginTransaction().replace(android.R.id.content, WeatherFragment.newInstance()).commit()
+//    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         supportFragmentManager.beginTransaction().replace(android.R.id.content, WeatherFragment.newInstance()).commit()
     }
 
